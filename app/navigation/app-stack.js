1335ba7990eb890ce596875d3b797385856021e2
Amado Gonzalez <agonzalezr@grainchain.io>
2020-12-12 15:04:24 -0600

    Setup for MovieDetail screen

--- app/navigation/app-stack.js
+++ app/navigation/app-stack.js
@@ -1,13 +1,14 @@
 import {createStackNavigator} from '@react-navigation/stack';
 import React from 'react';
 import ROUTES from './routes';
-import {MoviesList} from '../scenes';
+import {MovieDetail, MoviesList} from '../scenes';
 
 const Stack = createStackNavigator();
 
 const AppStack = () => (
   <Stack.Navigator>
     <Stack.Screen name={ROUTES.MOVIES_LIST} component={MoviesList} />
+    <Stack.Screen name={ROUTES.MOVIE_DETAIL} component={MovieDetail} />
   </Stack.Navigator>
 );
 
---
+++ app/scenes/movie-detail.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View} from 'react-native';
+
+const MovieDetail = ({}) => {
+  return <View />;
+};
+
+MovieDetail.propTypes = {};
+
+export default MovieDetail;
